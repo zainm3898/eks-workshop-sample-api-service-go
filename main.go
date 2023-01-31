b9049378c366f53c3008a1a9045b1c4a6fd8bff0
zainm3898 <116365519+zainm3898@users.noreply.github.com>
2023-01-31 15:44:01 +0500

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Zain"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
